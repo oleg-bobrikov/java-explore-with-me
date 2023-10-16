cbbcd825b0aedc2c2ea8fff1fcb22082af32dd66
Oleg Bobrikov <bobrikov@yandex.ru>
2023-10-17 01:17:26 +0300

    feature: stats-server

--- stats/stats-dto/src/main/java/ru/practicum/ewm/stats/dto/EndpointHitRequestDto.java
+++ stats/stats-dto/src/main/java/ru/practicum/ewm/stats/dto/EndpointHitRequestDto.java
@@ -1,15 +1,29 @@
 package ru.practicum.ewm.stats.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
+import org.jetbrains.annotations.NotNull;
 
 import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
 
-@Value
+import static ru.practicum.ewm.common.Constant.DATE_TIME_PATTERN;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class EndpointHitRequestDto {
-    @NotBlank String app;
-    @NotBlank String uri;
-    @NotBlank String ip;
+    @NotBlank
+    private String app;
 
-    @LocalDateTimeConstraint
-    String timestamp;
+    @NotBlank
+    private String uri;
+
+    @NotBlank
+    private String ip;
+
+    @NotNull
+    @JsonFormat(pattern = DATE_TIME_PATTERN)
+    private LocalDateTime timestamp;
 }
