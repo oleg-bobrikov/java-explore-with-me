0312a895f3e35da889866880c9c21dd7013dd723
Oleg Bobrikov <bobrikov@yandex.ru>
2023-10-17 01:29:23 +0300

    refactor: remove not necessary objects

--- stats/stats-server/src/main/java/ru/practicum/ewm/stats/endpointhit/EndPointHitMapper.java
+++ stats/stats-server/src/main/java/ru/practicum/ewm/stats/endpointhit/EndPointHitMapper.java
@@ -2,11 +2,10 @@ package ru.practicum.ewm.stats.endpointhit;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import ru.practicum.ewm.common.LocalDateTimeConverter;
 import ru.practicum.ewm.stats.dto.EndpointHitAnswerDto;
 import ru.practicum.ewm.stats.dto.EndpointHitRequestDto;
 
-@Mapper(imports = LocalDateTimeConverter.class, componentModel = "spring")
+@Mapper(componentModel = "spring")
 public interface EndPointHitMapper {
     EndpointHitAnswerDto toDto(EndpointHit endpointHit);
 
