6939864b73c8a3f9653e25f55bc2871c8f774b09
Oleg Bobrikov <bobrikov@yandex.ru>
2023-10-31 23:44:17 +0300

    fix: EventServiceImpl

--- main-service/src/main/java/ru/practicum/ewm/dto/validator/NotBlankOrNullValidator.java
+++ main-service/src/main/java/ru/practicum/ewm/dto/validator/NotBlankOrNullValidator.java
@@ -5,10 +5,6 @@ import javax.validation.ConstraintValidatorContext;
 
 public class NotBlankOrNullValidator implements ConstraintValidator<NotBlankOrNull, String> {
     @Override
-    public void initialize(NotBlankOrNull constraintAnnotation) {
-    }
-
-    @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
         return value == null || !value.trim().isEmpty();
     }
