74471269922206649c388b43759d8ba012787782
Oleg Bobrikov <bobrikov@yandex.ru>
2023-11-03 23:06:15 +0300

    refactor:

--- main-service/src/main/java/ru/practicum/ewm/dto/EventDto.java
+++ main-service/src/main/java/ru/practicum/ewm/dto/EventDto.java
@@ -1,20 +1,14 @@
 package ru.practicum.ewm.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import ru.practicum.ewm.dto.validator.LocalDateTimeIsAfter2Hours;
 import ru.practicum.ewm.model.Category;
 import ru.practicum.ewm.model.Location;
 import ru.practicum.ewm.model.User;
 
-import javax.validation.constraints.*;
-import javax.validation.Valid;
 import java.time.LocalDateTime;
-
-import static ru.practicum.ewm.common.Constant.DATE_TIME_PATTERN;
 
 @Getter
 @AllArgsConstructor
--- main-service/src/main/java/ru/practicum/ewm/mapper/EventMapper.java
+++ main-service/src/main/java/ru/practicum/ewm/mapper/EventMapper.java
@@ -5,13 +5,11 @@ import org.mapstruct.Mapping;
 import ru.practicum.ewm.dto.*;
 import ru.practicum.ewm.model.*;
 
-import java.time.LocalDateTime;
-
 @Mapper(uses = {CategoryMapper.class,
         UserMapper.class,
         LocationMapper.class}, componentModel = "spring")
 public interface EventMapper {
-    @Mapping(target = "id",ignore = true)
+    @Mapping(target = "id", ignore = true)
     @Mapping(target = "createdOn", ignore = true)
     @Mapping(target = "publishedOn", ignore = true)
     @Mapping(target = "state", expression = "java(Event.State.PENDING)")
@@ -20,6 +18,7 @@ public interface EventMapper {
     EventShortDto toEventShortDto(Event event, long confirmedRequests, long views);
 
     EventFullDto toEventFullDto(Event event, long confirmedRequests, long views);
+
     @Mapping(target = "initiator", source = "initiator")
     @Mapping(target = "category", source = "category")
     @Mapping(target = "location", source = "location")
